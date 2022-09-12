b2e54590d13e8b06180f664ac81d8d182bd4cb2b
devHspro <devhspro@gamail.com>
2022-09-12 21:41:46 +0800

    V-1.0

---
+++ src/main/java/com/hspro/Demo.java
@@ -0,0 +1,11 @@
+package com.hspro;
+
+/**
+ * @author: devhspro@gmail.com
+ * @Date: 2022/9/12
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
