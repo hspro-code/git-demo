eeac83bc13c2766c833871f638abb0a2116d8979
devHspro <devhspro@gamail.com>
2022-09-12 21:49:42 +0800

    V-1.1

--- src/main/java/com/hspro/Demo.java
+++ src/main/java/com/hspro/Demo.java
@@ -6,6 +6,6 @@ package com.hspro;
  */
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world2");
     }
 }
